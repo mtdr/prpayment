5ef328fb264fa2db5aa71a980533f4cab4187080
mtdr <paul.osmolovsky@gmail.com>
2020-09-04 20:43:39 +0300

    Initial commit

---
+++ src/main/java/com/edu/mtdr/prpayment/PaymentProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.edu.mtdr.prpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentProcessorApplication.class, args);
+    }
+
+}
